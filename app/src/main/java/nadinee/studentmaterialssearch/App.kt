5b6ff4e011aa390f25caa4b24febf832d45ddf87
NADEZHDA <128965468+Nadineenik@users.noreply.github.com>
2025-11-06 16:03:41 +0300

    обновлен интерфейс с отображением имени и редактированием профиля. в экране логина заполнена заглушка имени

--- app/src/main/java/nadinee/studentmaterialssearch/App.kt
+++ app/src/main/java/nadinee/studentmaterialssearch/App.kt
@@ -3,35 +3,39 @@ package nadinee.studentmaterialssearch
 
 import android.app.Application
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import nadinee.studentmaterialssearch.data.AppDatabase
 
+// App.kt
 class App : Application() {
     companion object {
         val database: AppDatabase by lazy {
-            // Это выполнится при первом обращении
             Room.databaseBuilder(
                 instance.applicationContext,
                 AppDatabase::class.java,
                 "student_app.db"
             )
-                .fallbackToDestructiveMigration()
+                .addMigrations(MIGRATION_1_2 as Migration)
                 .build()
         }
 
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE users ADD COLUMN name TEXT NOT NULL DEFAULT 'Без имени'")
+                db.execSQL("ALTER TABLE users ADD COLUMN interests TEXT NOT NULL DEFAULT ''")
+            }
+        }
+
         lateinit var instance: App
-            private set
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
-
-        // Предзагрузка (опционально)
-        GlobalScope.launch(Dispatchers.IO) {
-            database // просто вызов, чтобы инициализировать
-        }
+        GlobalScope.launch(Dispatchers.IO) { database }
     }
 }
